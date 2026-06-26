5591b172b7162454aef50fc75af09164fc8f9abe
ricardoemf03Incan <ricardoemf03@gmail.com>
2026-06-26 13:46:53 -0600

    project removed

--- CaCuSibAutoPlan.Script/Script.cs
+++ CaCuSibAutoPlan.Script/Script.cs
@@ -7,9 +7,9 @@ using CaCuSibAutoPlan.Views;
 using VMS.TPS.Common.Model.API;
 using VMS.TPS.Common.Model.Types;
 
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
-[assembly: AssemblyInformationalVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyInformationalVersion("1.0.0.3")]
 [assembly: ESAPIScript(IsWriteable = true)]
 
 namespace VMS.TPS
