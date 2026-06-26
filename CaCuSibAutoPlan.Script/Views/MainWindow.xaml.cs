c01f3e8af6e223aa135a5906118aecfe1f4756eb
ricardoemf03Incan <ricardoemf03@gmail.com>
2026-06-26 13:51:48 -0600

    Interface and shared were removed

--- CaCuSibAutoPlan.Script/Views/MainWindow.xaml.cs
+++ CaCuSibAutoPlan.Script/Views/MainWindow.xaml.cs
@@ -5,5 +5,10 @@ namespace CaCuSibAutoPlan.Views
     public partial class MainWindow : Window
     {
         public MainWindow() { InitializeComponent(); }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
